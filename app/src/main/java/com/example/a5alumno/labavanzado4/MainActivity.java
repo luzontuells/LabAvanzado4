bfd007c2d22c562d59686b090f9eef495851ba89
Raul Garcia <luzontuells@gmail.com>
2016-11-28 10:28:53 +0100

    first dev commit

--- app/src/main/java/com/example/a5alumno/labavanzado4/MainActivity.java
+++ app/src/main/java/com/example/a5alumno/labavanzado4/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.a5alumno.labavanzado4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnQueryDb = (Button) this.findViewById(R.id.btn_query_db);
+        btnQueryDb.setOnClickListener(this);
+
+        final Button btnAddDb = (Button) this.findViewById(R.id.btn_add_db);
+        btnAddDb.setOnClickListener(this);
+
+        final Button btnDeleteDb = (Button) this.findViewById(R.id.btn_delete_db);
+        btnDeleteDb.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_query_db:
+
+                break;
+            case R.id.btn_add_db:
+
+                break;
+            case R.id.btn_delete_db:
+
+                break;
+            default:
+
+                break;
+        }
     }
 }
